e7078863855a8d1d16dc85f5d6cc33a95cbf0d11
Muhammad Qasim <muhammadqasim1014@gmail.com>
2025-04-28 15:01:23 +0500

    tabs

---
+++ src/components/TabButtons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TabButtons = ({ children, onSelect, isSelected }) => {
+
+    return (
+
+        <li>
+            <button className={isSelected ? "active": ''} onClick={onSelect}>{children}</button>
+        </li>
+
+    )
+}
+
+export default TabButtons
